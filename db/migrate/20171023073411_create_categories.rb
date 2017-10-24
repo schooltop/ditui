11e1a03b29be34c970d53135c4f07b02eefb65ec
jamst <jamstjamst@163.com>
2017-10-24 14:10:11 +0800

    merge

--- db/migrate/20171023073411_create_categories.rb
+++ db/migrate/20171023073411_create_categories.rb
@@ -1,6 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
+      t.string  :name
+      t.integer :parent_id
+      t.timestamps  	
     end
   end
 end

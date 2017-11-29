0e278ef18f9ca26c91983793c779149914111bce
jamst <jamstjamst@163.com>
2017-11-29 22:42:28 +0800

    个人中心完善

--- app/controllers/web/center_controller.rb
+++ app/controllers/web/center_controller.rb
@@ -11,12 +11,16 @@ class Web::CenterController < Web::BaseController
 		@customers_vendors = @customers_vendors.select{|_|_.vendor.created_at >= params[:time] } if params[:time].present? 
 	end
 
-	def my_visite
-
+	def my_visit
+        @my_from = current_user.my_from
+		@my_from = @my_from.select{|_|_.name == params[:name] } if params[:name].present?
+		@my_from = @my_from.select{|_|_.created_at >= params[:time] } if params[:time].present? 
 	end
 
 	def my_search
-
+        @my_search = current_user.gps
+		@my_search = @my_search.select{|_|_.customer.name == params[:name] } if params[:name].present?
+		@my_search = @my_search.select{|_|_.created_at >= params[:time] } if params[:time].present? 
 	end
 
 end
